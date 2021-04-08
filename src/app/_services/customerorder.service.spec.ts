d2d700df1f94d207bc3d5304e23757149bb8b914
Aleksa Pavlovic <ap20160012@gmail.com>
2021-04-08 16:57:36 +0200

    fixed test

--- src/app/_services/customerorder.service.spec.ts
+++ src/app/_services/customerorder.service.spec.ts
@@ -1,14 +1,12 @@
 import { HttpClient } from "@angular/common/http";
 import { OrderService } from "./customerorder.service"
-import { MocksService } from "./mocks.service";
 
 describe('OrderService', () =>{
     let service: OrderService;
 
     beforeEach(() => {
         let http: HttpClient;
-        let mock: MocksService;
-        service = new OrderService(http,mock);
+        service = new OrderService(http);
     })
 
     it('should have no order items to start', () =>{
